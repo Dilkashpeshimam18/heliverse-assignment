79fa7921da431f735dbc319af7d91a0fe408f57e
Dilkash Peshimam <dilkashpeshimam@gmail.com>
2023-12-01 19:50:35 +0530

    implemented pagination logic

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,8 +1,20 @@
+const User=require('../models/user')
 
 exports.getAllUser = async (req, res) => {
     try {
+        const userId = req.user.id;
+        const page = parseInt(req.query.page) || 1;
+        const rowPerPage = parseInt(req.query.rowPerPage) || 10; 
+        const skip = (page - 1) * rowPerPage;
+    
+        const total_user = await User.countDocuments({ userId: userId });
+    
         const users = await User.find()
-        res.status(200).json({ success: true, users })
+            .skip(skip)
+            .limit(rowPerPage)
+            .exec();
+    
+        return res.status(200).json({ users, lastPage: Math.ceil(total_user / rowPerPage) });
     } catch (err) {
         console.log(err)
         res.status(500).json({ error: err });
